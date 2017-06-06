b4ef8677441426557a1a2a0e338acc4dbb3c864d
frisjon <frisjon0@gmail.com>
2017-06-06 19:22:11 -0300

    Agrego pruebas de Goku

---
+++ test/testPersonajes/TestGoku.java
@@ -0,0 +1,76 @@
+package testPersonajes;
+
+import personajes.ErrorNoPuedeCambiarEstado;
+import personajes.Goku;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+public class TestGoku{
+    private Goku goku;
+
+    @Before
+    public void setUp() {
+        goku = new Goku();
+    }
+    
+    @Test
+    public void test01CrearGoku() {
+        assertEquals("Goku", goku.getNombre());
+    }
+    
+    @Test
+    public void test02GokuEmpiezaEnEstado1() {
+        assertEquals(1, goku.getIdEstado());
+    }
+    
+    @Test
+    public void test03CambiarGokuAEstado1() {
+        goku.cambiarAEstado1();
+        assertEquals(1, goku.getIdEstado());
+    }
+    
+    @Test
+    public void test04CambiarGokuAEstado2() {
+        try {
+            goku.agregarKi(20);
+            goku.cambiarAEstado2();
+        } catch (ErrorNoPuedeCambiarEstado e1) {
+            fail("Error Inesperado");
+        }
+        assertEquals(2, goku.getIdEstado());
+    }
+    
+    @Test (expected=ErrorNoPuedeCambiarEstado.class)
+    public void test05CambiarGokuAEstado2KiInsuficienteLanzaExcepcion() throws ErrorNoPuedeCambiarEstado {
+        try {
+            goku.cambiarAEstado2();
+        } catch (ErrorNoPuedeCambiarEstado e1) {
+            throw e1;
+        }
+        fail("Error Inesperado");
+    }
+    
+    @Test
+    public void test06CambiarGokuAEstado3() {
+        try {
+            goku.agregarKi(50);
+            goku.cambiarAEstado3();
+        } catch (ErrorNoPuedeCambiarEstado e1) {
+            fail("Error Inesperado");
+        }
+        assertEquals(3, goku.getIdEstado());
+    }
+    
+    @Test (expected=ErrorNoPuedeCambiarEstado.class)
+    public void test07CambiarGokuAEstado3KiInsuficienteLanzaExcepcion() throws ErrorNoPuedeCambiarEstado {
+        try {
+            goku.cambiarAEstado3();
+        } catch (ErrorNoPuedeCambiarEstado e1) {
+            throw e1;            
+        }
+        fail("Error Inesperado");
+    }
+}
